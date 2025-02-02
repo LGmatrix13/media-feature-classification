c4bd45c10bce38714b0702e0ceea8362ab4b2c7d
Liam Grossman <liam.j.grossman@gmail.com>
2025-02-02 15:29:34 -0500

    Update elastic.py

--- analysis/utilities/elastic.py
+++ analysis/utilities/elastic.py
@@ -4,6 +4,26 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch("http://localhost:9200")
 
 class Elastic:
+    def __init__(self):
+        Elastic.__create_index("movies")
+        Elastic.__create_index("books")
+        Elastic.__create_index("music")
+
+    @staticmethod
+    def __create_index(index_name: str):
+        if not es.indices.exists(index=index_name):
+            es.indices.create(
+                index=index_name,
+                body={
+                    "mappings": {
+                        "properties": {
+                            "text": {"type": "text"},
+                            "embedding": {"type": "dense_vector", "dims": 128} 
+                        }
+                    }
+                }
+            )
+            
     @staticmethod
     def __index(index_name: str, series: pd.Series):
         body = series.to_dict()
@@ -11,13 +31,13 @@ class Elastic:
             index=index_name,
             body = body
         )
-    @staticmethod
+
     def index_movie(movie: pd.Series):    
         Elastic.__index("movies", movie)
-    @staticmethod   
+
     def index_book(book: pd.Series):
         Elastic.__index("books", book)
-    @staticmethod
+
     def index_music(music: pd.Series):
         Elastic.__index(index_name="music", series=music)
 
