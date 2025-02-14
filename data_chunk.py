c0a03c64ccbc38c3e5bb1e9d2b60b25fa8614aec
Liam Grossman <liam.j.grossman@gmail.com>
2025-02-14 15:53:34 -0500

    refine chunking code by column. revert to word2vec

--- data_chunk.py
+++ data_chunk.py
@@ -1,12 +1,53 @@
 import pandas as pd
 
-def main():
-    chunksize = 10 ** 6
+def chunk_movies():
+    chunksize = 10 ** 4
     chunk_idx = 0
-    for chunk in pd.read_csv('./data/raw/music.csv', chunksize=chunksize, usecols=['title', 'tag', 'artist', 'year', 'views', 'features', 'lyrics', 'id', 'language_cld3']):
+    metadata = pd.DataFrame()
+    overview = pd.DataFrame()
+    columns = ['id', 'title', 'vote_average', 'vote_count', 'status', 'release_date',
+       'revenue', 'runtime', 'adult', 'backdrop_path', 'budget', 'homepage',
+       'imdb_id', 'original_language', 'original_title', 'overview',
+       'popularity', 'poster_path', 'tagline', 'genres',
+       'production_companies', 'production_countries', 'spoken_languages',
+       'keywords']
+    for chunk in pd.read_csv('./data/raw/movies.csv', chunksize=chunksize, usecols=columns):
         print(f"Running chunk {chunk_idx}...")
-        chunk[chunk['language_cld3'] == 'en'].to_parquet(f'./data/raw/music_{chunk_idx}.parquet')
+        english_only = chunk['original_language'] == 'en'
+        metadata_columns = [
+            'id', 'title', 'vote_average', 'vote_count', 'status', 'release_date',
+            'revenue', 'runtime', 'adult', 'backdrop_path', 'budget', 'homepage',
+            'imdb_id', 'original_language', 'original_title',
+            'popularity', 'poster_path', 'tagline', 'genres',
+            'production_companies', 'production_countries', 'spoken_languages',
+            'keywords'
+            ]
+        metadata_chunk = chunk.loc[english_only, metadata_columns]
+        metadata = pd.concat([metadata, metadata_chunk])
+        overview_chunk = chunk.loc[english_only, ['overview']]
+        overview = pd.concat([overview, overview_chunk])
         chunk_idx += 1
 
-if __name__ == "main":
+    metadata.to_parquet('./data/raw/movies_metadata.parquet')
+    overview.to_parquet('./data/raw/movies_overview.parquet')
+
+def chunk_music():
+    chunksize = 10 ** 6
+    chunk_idx = 0
+    metadata = pd.DataFrame()
+    lyrics = pd.DataFrame()
+
+    for chunk in pd.read_csv('./data/raw/music.csv', chunksize=chunksize, usecols=['title', 'tag', 'artist', 'year', 'views', 'features', 'lyrics', 'id', 'language_cld3']):
+        print(f"Running chunk {chunk_idx}...")
+        english_only = chunk['language_cld3'] == 'en'
+        metadata_chunk = chunk.loc[english_only, ['title', 'tag', 'artist', 'year', 'views', 'features']]
+        metadata = pd.concat([metadata, metadata_chunk])
+        lyrics_chunk = chunk.loc[english_only, ['lyrics','language_cld3']]
+        lyrics = pd.concat([lyrics, lyrics_chunk])
+        chunk_idx += 1
+
+    metadata.to_parquet('./data/raw/music_metadata.parquet')
+    lyrics.to_parquet('./data/raw/music_lyrics_metadata.parquet')
+
+if __name__ == "__main__":
     main()
--- vectorize.py
+++ vectorize.py
@@ -1,31 +1,41 @@
 from argparse import ArgumentParser, Namespace
+import nltk
 import pandas as pd
-import ollama
+import gensim.downloader as api 
+import numpy as np
+from nltk.tokenize import word_tokenize
 
-def embed(text: str):
-    return ollama.embeddings(model='all-minilm', prompt=text).get('embedding')
+nltk.download('punkt')
+
+def embed(text: str, model):
+    words = word_tokenize(text)
+    vectors = [model[word] for word in words if word in model]
+    if vectors:
+        return np.mean(vectors, axis=0).tolist()
+    return None
 
 def main(args: Namespace):
     input_df = pd.read_parquet(args.input_file)
+    model = api.load("word2vec-google-news-300")
     output_data = []
     
     for index, row in input_df.iterrows():
-        print(f"Processing row {index}")
+        print(f"Processing row {index}...")
         if row[args.text_column] is None:
-            output_data.append({"index": index, "vector": None})  # Convert numpy array to list
+            output_data.append({ "pk": row[args.pk_columnn], "vector": None })
         else:
-            vector = embed(row[args.text_column])
-            output_data.append({"index": index, "vector": vector})
+            vector = embed(row[args.text_column], model)
+            output_data.append({ "pk": row[args.pk_columnn], "vector": vector })
 
-    # Create output DataFrame and save
     output_df = pd.DataFrame(output_data)
     output_df.to_parquet(args.output_file, index=False)
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Run a vectorization process on a given dataset using a pretrained Doc2Vec model.")
+    parser = ArgumentParser(description="Run a vectorization process on a given dataset using a pretrained Word2Vec model.")
     parser.add_argument("input_file", type=str, help="Path to input dataset")
+    parser.add_argument("pk_columnn", type=str, help="id/primary key of the input dataset")
     parser.add_argument("text_column", type=str, help="Column name containing text data")
-    parser.add_argument("model_path", type=str, help="Path to the pretrained Doc2Vec model")
+    parser.add_argument("model_path", type=str, help="Path to the pretrained Word2Vec model")
     parser.add_argument("output_file", type=str, help="Path to export output dataset")
     args = parser.parse_args()
     main(args)
