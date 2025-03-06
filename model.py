2494c6eb2b0c3dbeb188e201292b87518df8c49d
Liam Grossman <liam.j.grossman@gmail.com>
2025-03-06 13:06:57 -0500

    start model

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
+import numpy as np
 
 print("Reading data...")
 df = pd.read_parquet("./data/raw/music_lyrics_vectors.parquet")
-X = df['vector']
+X = np.array(df['vector'].apply(lambda vector: list(vector)), dtype=object)
+
 print("Training model...")
 knn = NearestNeighbors(n_neighbors=3)
 knn.fit(X)
